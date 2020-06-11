1675af17fea6f2ae3e8fee79b872fb7de0c198a1
laurie kern <laurie.kern.ctr@us.af.mil>
2020-06-11 15:02:44 -0500

    Add project files.

---
+++ AdminLTE.Core3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminLTE.Core3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
